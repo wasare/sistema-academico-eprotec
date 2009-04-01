ff7140a2f9e19c0b2c4e9ea426d1a4d99c0101a2
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-04-01 11:17:59 +0000

    Implementação da classe de exportação para XLS; relatório alunos matriculados com opção de exportar XLS.

---
+++ trunk/app/relatorios/xls_alunos.php
@@ -0,0 +1,140 @@
+<?php
+
+header("Cache-Control: no-cache");
+require("../../lib/common.php");
+require("../../configuracao.php");
+require("../../lib/adodb/adodb.inc.php");
+require("../../lib/excel.inc.php");
+
+
+$periodo    = $_POST["periodo1"];
+$aluno      = $_POST["aluno"];
+$curso      = $_POST["codigo_curso"];
+$resp_nome  = $_POST["resp_nome"];
+$resp_cargo = $_POST["resp_cargo"];
+$turma      = $_POST["turma"];
+
+
+$colunas[] = "Nome";
+
+$sql = " SELECT DISTINCT p.nome || ' - ' || p.id as \"Nome\" ";
+
+
+if (isset($_POST["turma2"])) {
+
+	$colunas[] = "Turma";
+	$sql .= ', t.turma as "Turma" ';
+	$condicao_turma = "AND t.ref_curso = c.ref_curso AND t.ref_pessoa = p.id";
+	$tabela_contrato = ", public.contratos t";
+}
+
+if ($turma != '') {
+
+	$condicao_turma .= " AND t.turma = '$turma' ";
+	$tabela_contrato = ", public.contratos t ";
+}
+
+if (isset($_POST["pai"])) {
+
+	$colunas[] = "Pai";
+	$sql .= ', f.pai_nome as "Pai"';
+	$tabela_filiacao = "LEFT OUTER JOIN filiacao f ON(p.ref_filiacao = f.id)";
+}
+
+if (isset($_POST["mae"])) {
+
+	$colunas[] = "Mãe";
+	$sql .= ', f.mae_nome as "Mae" ';
+	$tabela_filiacao = "LEFT OUTER JOIN filiacao f ON(p.ref_filiacao = f.id)";
+}
+
+if (isset($_POST["endereco"])) {
+
+	$colunas[] = "Endereço";
+	$sql .= ", p.rua || '  ' || CASE WHEN p.complemento IS NULL THEN ' ' ELSE p.complemento END AS \"Endereço\"";
+}
+
+if (isset($_POST["bairro"])) {
+
+	$colunas[] = "Bairro";
+	$sql .= ', p.bairro as "Bairro"';
+}
+
+if (isset($_POST["cidade"])) {
+
+	$colunas[] = "Cidade";
+	$sql .= ', m.nome || \'-\' || m.ref_estado as "Cidade"';
+	$condicao_municipio = " p.ref_cidade = m.id AND ";
+	$tabela_municipio = ", public.aux_cidades m";
+}
+
+if (isset($_POST["cep"])) {
+
+	$colunas[] = "CEP";
+	$sql .= ', p.cep as "CEP"';
+}
+
+if (isset($_POST["telefone"])) {
+
+	$colunas[] = "Telefone";
+	$sql .= ', p.fone_particular as "Tel. Part."
+        , p.fone_profissional as "Tel. Prof."
+        , p.fone_celular as "Tel. Cel."
+        , p.fone_recado as "Tel. Rec."
+      ';
+}
+
+if (isset($_POST["rg"])){
+
+	$colunas[] = "RG";
+	$sql .= ', p.rg_numero as "RG"';
+}
+
+if (isset($_POST["cpf"])){
+
+	$colunas[] = "CPF";
+	$sql .= ', p.cod_cpf_cgc as "CPF"';
+}
+
+if (isset($_POST["sexo"])){
+
+	$colunas[] = "Sexo";
+	$sql .= ', p.sexo as "Sexo"';
+}
+
+if (isset($_POST["data_nascimento"])) {
+
+	$colunas[] = "Data Nascimento";
+	$sql .= ', to_char(p.dt_nascimento, \'DD/MM/YYYY\') as "Data de Nascimento"';
+}
+
+$sql .= "
+  FROM
+  pessoas p $tabela_filiacao, matricula c $tabela_contrato $tabela_municipio
+  WHERE
+  c.ref_periodo = '$periodo' AND ";
+
+if ($curso != '') {
+	$sql .= " c.ref_curso = '$curso' AND";
+}
+
+$sql .= $condicao_municipio;
+
+if ($aluno != '') {
+	$sql .= " p.id = '$aluno' AND ";
+}
+
+$sql .= " c.ref_pessoa = p.id $condicao_turma ORDER BY 2";
+
+$sql = 'SELECT * FROM ('. $sql .') AS T1 ORDER BY lower(to_ascii("Nome"));';
+
+
+$Conexao = NewADOConnection("postgres");
+$Conexao->PConnect("host=$host dbname=$database user=$user password=$password");
+
+$Result1 = $Conexao->Execute($sql);
+
+
+$gerar= new sql2excel($colunas,$sql);
+
+?>
