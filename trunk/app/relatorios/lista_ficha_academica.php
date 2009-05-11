fb4ef099f76a5303f5a4f5a59596adbba7cafb0f
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-05-11 12:09:13 +0000

    Implementado funcionalidade no relatório Ficha Academica. fecha #71

--- trunk/app/relatorios/lista_ficha_academica.php
+++ trunk/app/relatorios/lista_ficha_academica.php
@@ -7,12 +7,9 @@
   require("../../lib/adodb/adodb.inc.php");
   require("../../lib/adodb/tohtml.inc.php");
   
-  //Criando a classe de conexão ADODB
-  $Conexao = NewADOConnection("postgres");
-  
-  //Setando como conexão persistente
-  $Conexao->PConnect("host=$host dbname=$database user=$user password=$password");
 
+  $Conexao = NewADOConnection("postgres");
+  $Conexao->PConnect("host=$host dbname=$database user=$user password=$password");
 
 	$ras = $_GET['aluno'];
 	
@@ -60,7 +57,7 @@
 ?>
 <html>
 <head>
-<title>Web Di&aacute;rio</title>
+<title>SA</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link href="../../Styles/style.css" rel="stylesheet" type="text/css">
 <body>
@@ -68,7 +65,7 @@
 <div align="center" style="text-align:center; font-size:12px;">
 	<img src="../../images/armasbra.jpg" width="57" height="60"><br />
 	MEC-SETEC<br />
-	CENTRO FEDERAL DE EDUCAÇÃO TECNOLÓGICA DE BAMBUÍ-MG<br />
+	CENTRO FEDERAL DE EDUCA&Ccedil;&Atilde;O TECNOL&Oacute;GICA DE BAMBU&Iacute;-MG<br />
     SETOR DE REGISTROS ESCOLARES
     <br /><br /><br />
 </div>
@@ -87,8 +84,20 @@
     <th width="15%"><div align="center"><font color="#FFFFFF"><b>CH Realizada</b></font></div></th>
     <th width="15%"><div align="center"><font color="#FFFFFF"><b>CH Prevista</b></font></div></th>
   </tr>
-  <?php	
-  	
+<?php	
+
+//VARIAVEIS --
+
+//nota total aprovado
+$notaAprovado = 0;
+//contador
+$contAprovado = 0;
+//percentual de faltas
+$percFaltasAprovado = 0;
+//carga horaria realizada
+$chRealizadaAprovado = 0;
+  
+
 while(!$Result1->EOF) {
 
 	$iddisc = $Result1->fields[0];
@@ -99,23 +108,12 @@ while(!$Result1->EOF) {
     $aulaprev = $Result1->fields[3];
     $oferecida = $Result1->fields[8];
     
-	//CODIGO RETIRADO DA PAPELETA papeleta.php - NOTA FINAL
-	
-   /* if($Result1->fields[6] != 0) { 
-		$notafinal = getNumeric2Real($Result1->fields[6]); 
-	}
-	else { 
-		$notafinal = $Result1->fields[6];
-	}*/
-	
 	
 	$notafinal = $Result1->fields[6];
    	
 	if($notafinal == ''){
 		$notafinal = ' - ';
-	}
-	//FIM CODIGO PAPELETA
-    
+	}  
                
     if ($faltasmateria == 0) { 
 		$faltasmateria='-';
@@ -155,8 +153,18 @@ while(!$Result1->EOF) {
 		$fcolor = '#FF0000';
 	}
 	else {
+		
+		$contAprovado++;
+		//total notas aprovado
+		$notaAprovado += $notafinal;
+		//total percentual de faltas
+		$percFaltasAprovado += $stfaltas;
+		//Total carga horaria realizada
+		$chRealizadaAprovado += $res;
+		
 		$fcolor = '#000000';
 	}
+	
 	
 	//VERIFICA SE A CARGA HORARIA REALIZADA ESTA NULA
 	if ($res == "") { 
@@ -186,9 +194,47 @@ while(!$Result1->EOF) {
 	$Result1->MoveNext();
 	
 }//FIM WHILE
+
+
+//INFORMACOES --
+
+//Media nas disciplinas aprovadas
+$notaMediaAprovado = $notaAprovado / $contAprovado;
+
+//Convertendo para o padrao decimal - Media nas disciplinas aprovadas
+$notaMediaAprovado = number_format($notaMediaAprovado,'2',',','.');
+
+//Media percentual de faltas das disciplinas aprovadas
+$percFaltasAprovado = $percFaltasAprovado / $contAprovado;
+
+//Convertendo para o padrao decimal - Media percentual de faltas das disciplinas aprovadas
+$percFaltasAprovado = number_format($percFaltasAprovado,'2',',','.');
                  
 ?>
 </table>
 </div>
+
+<br /><br />
+
+<table width="379" border="0" cellspacing="0" cellpadding="0" class="tabela_relatorio">
+  <tr bgcolor="666666">
+    <th height="24" colspan="2">
+    	<b>Informa&ccedil;&otilde;es:</b><br>    
+    </th>
+  </tr>
+  <tr>
+    <td width="321">M&eacute;dia da nota nas disciplinas aprovadas:</td>
+    <td width="58" align="right">&nbsp;<?php echo $notaMediaAprovado; ?></td>
+  </tr>
+  <tr>
+    <td>M&eacute;dia percentual de faltas das disciplinas aprovadas: </td>
+    <td align="right">&nbsp;<?php echo $percFaltasAprovado . ' %'; ?></td>
+  </tr>
+  <tr>
+    <td>Total carga hor&aacute;ria realizada nas disciplinas aprovadas: </td>
+    <td align="right">&nbsp;<?php echo $chRealizadaAprovado;?></td>
+  </tr>
+</table>
+
 </body>
 </html>
