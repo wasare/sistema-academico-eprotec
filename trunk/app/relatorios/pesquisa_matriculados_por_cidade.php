a0f06a098444da4800ad3e8ac5833f5253442a64
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-08-10 13:18:11 +0000

    Relatorios de egressos. Aplicacao da fabrica de conexao, cabecalho e carimbo do relatorio de egressos, declaracao de matricula.

--- trunk/app/relatorios/pesquisa_matriculados_por_cidade.php
+++ trunk/app/relatorios/pesquisa_matriculados_por_cidade.php
@@ -4,7 +4,7 @@ header("Cache-Control: no-cache");
 
 //INCLUSAO DE BIBLIOTECAS
 require("../../lib/common.php");
-require("../../configuracao.php");
+require("../../configs/configuracao.php");
 require("../../lib/adodb/adodb.inc.php"); 
 
 
@@ -29,7 +29,7 @@ if (!$Result1){
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <title>Lista alunos matriculados</title>
-        <link href="../../Styles/formularios.css" rel="stylesheet" type="text/css" />
+        <link href="../../public/styles/formularios.css" rel="stylesheet" type="text/css" />
         <style type="text/css">
             <!--
             .style1 {
@@ -95,17 +95,17 @@ if (!$Result1){
                 <tr>
                     <td width="68" align="center">
                         <label class="bar_menu_texto">
-                            <input name="input" type="image" src="../../images/icons/print.jpg" alt="Exibir" onclick="submit_opt('lista_matriculados_por_cidade.php');" />
+                            <input name="input" type="image" src="../../public/images/icons/print.jpg" alt="Exibir" onclick="submit_opt('lista_matriculados_por_cidade.php');" />
                             <br />
                     Exibir</label>      </td>
                     <td width="73" align="center">
                         <label class="bar_menu_texto">
-                            <input type="image" name="imageField" id="imageField" src="../../images/icons/pdf_icon.jpg" onclick="submit_opt('pdf_matriculados_por_cidade.php');" />
+                            <input type="image" name="imageField" id="imageField" src="../../public/images/icons/pdf_icon.jpg" onclick="submit_opt('pdf_matriculados_por_cidade.php');" />
                             <br />
                         Gerar PDF</label>
                     </td>
                     <td width="63" align="center">
-                        <label class="bar_menu_texto"> <a href="#" onclick="history.back(-1)" class="bar_menu_texto"> <img src="../../images/icons/back.png" alt="Voltar" width="20" height="20" /><br />
+                        <label class="bar_menu_texto"> <a href="#" onclick="history.back(-1)" class="bar_menu_texto"> <img src="../../public/images/icons/back.png" alt="Voltar" width="20" height="20" /><br />
                     Voltar</a> </label>
                     </td>
                 </tr>
