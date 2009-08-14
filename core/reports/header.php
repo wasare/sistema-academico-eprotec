843af55a5c4729e430bfe6b0473ff956ff0e0851
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-08-14 19:23:40 +0000

    Alteracao na classe header

--- core/reports/header.php
+++ core/reports/header.php
@@ -17,11 +17,10 @@ class header{
 
         $RsEmpresa = $conn->Execute("SELECT razao_social, sigla FROM configuracao_empresa WHERE id = 1");
 
-		$resp = '<div align="center" style="margin:8px;">
-                    <img src="'.$path_images.'logo.jpg" />
-                	<br /><br />
-					<font face="verdana" size="3">'.$RsEmpresa->fields[0].'</font>
-				</div>';
+		$resp = '<br />
+		         <img src="'.$path_images.'logo.jpg" />
+                 <br /><br />
+				 <font face="verdana" size="3">'.$RsEmpresa->fields[0].'</font>';
 			
         $conn->Close();
 
