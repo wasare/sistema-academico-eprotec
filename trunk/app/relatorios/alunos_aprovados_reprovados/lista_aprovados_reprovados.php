4c8937c0752e02742785dd7064fcd8f9dd48596a
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-08-17 10:40:29 +0000

    Aprimoramento do relatorio aprovados / reprovados.

--- trunk/app/relatorios/alunos_aprovados_reprovados/lista_aprovados_reprovados.php
+++ trunk/app/relatorios/alunos_aprovados_reprovados/lista_aprovados_reprovados.php
@@ -1,8 +1,13 @@
 <?php
   
-  require_once("../../../configs/configuracao.php");
-  require_once("../../../lib/adodb/tohtml.inc.php");
-  require_once("aprovados_reprovados.php");
+require_once("../../../configs/configuracao.php");
+require_once("../../../core/reports/header.php");
+require_once("../../../core/reports/carimbo.php");
+require_once("../../../lib/adodb/tohtml.inc.php");
+require_once("aprovados_reprovados.php");
+ 
+$header  = new header($param_conn);
+$carimbo = new carimbo($param_conn);
   
 ?>
 <html>
@@ -12,21 +17,26 @@
 <link href="../../../public/styles/style.css" rel="stylesheet" type="text/css">
 </head>
 <body bgcolor="#FFFFFF" marginwidth="20" marginheight="20">
-	<div style="width: 760px;">
-		<?php 
-			/**
-			 * FIXME: Inserir cabecalho e carimbo
-		 	*/
-	
-			//header
-		?>
+	<div style="width: 760px;" align="center">
+		<div align="center" style="text-align:center; font-size:12px;">
+        	<?php echo $header->get_empresa($PATH_IMAGES); ?>
+            <br /><br />
+        </div> 
 		<h2>RELAT&Oacute;RIO DE SITUA&Ccedil;&Atilde;O DE APROVA&Ccedil;&Atilde;O DE ALUNO(S)</h2>
 		<?php
 			echo $info;
   			rs2html($Result1, 'width="90%" cellspacing="0" border="0" class="tabela_relatorio" cellpadding="0"'); 
 		?>
-		<br><br>
-		<?php //carimbo ?>
+		<br /><br />
+		<div class="carimbo_box">
+           	_______________________________<br>
+			<span class="carimbo_nome">
+		   		<?php echo $carimbo->get_nome($_POST['carimbo']);?>
+			</span><br />
+			<span class="carimbo_funcao">
+		   		<?php echo $carimbo->get_funcao($_POST['carimbo']);?>
+			</span>
+	    </div>
 	</div>
 </body>
 </html>
--- trunk/app/relatorios/alunos_aprovados_reprovados/pdf_aprovados_reprovados.php
+++ trunk/app/relatorios/alunos_aprovados_reprovados/pdf_aprovados_reprovados.php
@@ -1,79 +1,18 @@
 <?php
 
-  require("../../../configs/configuracao.php");
-  require("../../../lib/adodb/tohtml.inc.php");
-  
-  $conn = new connection_factory($param_conn);
+require_once("../../../configs/configuracao.php");
+require_once("../../../core/reports/header.php");
+require_once("../../../core/reports/carimbo.php");
+require_once("../../../lib/adodb/tohtml.inc.php");
+require_once("aprovados_reprovados.php");
+ 
+$header    = new header($param_conn);
+$carimbo   = new carimbo($param_conn);
+$cabecalho = $header->get_empresa($PATH_IMAGES);  
 
-  if($_POST["periodo1"] != ''){  
-	  $periodo = $_POST["periodo1"];
-  }
-  else{
-	  $periodo = $_POST["periodo"];
-  }
-  
-  $curso      = $_POST["codigo_curso"];
-  $aluno      = $_POST["aluno"];
-  $situacao   = $_POST["aprovacao"]; //1 = aprovado, 2 = reprovado, 3 = aprovado e reprovado
-  $turma      = $_POST["turma"];
+//html2pdf
+ob_start();  
 
-  $sql = "
-  SELECT 
-  	t.turma as \"Turma\", 
-  	p.nome || ' (' || m.ref_pessoa || ') ' as \"Nome (Cód)\", 
-  	d.descricao_disciplina || ' (' || o.ref_disciplina || '/' || m.ref_disciplina_ofer || ') ' as \"Disciplina (Cód Disc/Diário) \",
-  	m.nota_final as \"Nota\", 
-  	m.num_faltas || ' (' || d.carga_horaria || ') ' as \"Falta (Carga Horaria)\"
-	
-  FROM 
-  	matricula m, pessoas p, disciplinas_ofer o, disciplinas d, public.contratos t
-  
-  WHERE
-  	m.ref_periodo = '$periodo' AND 
-  	m.ref_curso = '$curso' AND 
-  	t.ref_curso = m.ref_curso AND 
-  	t.ref_pessoa = p.id AND ";
-  
-  if ($turma != '') {
-  	$sql .= " t.turma = '$turma' AND ";
-  }
-  
-  if ($aluno != '') {
-  	$sql .= "m.ref_pessoa = '$aluno' AND ";
-  }
-
-  $sql .= "p.id = m.ref_pessoa AND m.ref_disciplina_ofer = o.id AND o.ref_disciplina = d.id	";
-
-  if ($situacao == '1') {
-  	$sql .= " AND (m.nota_final >= 60 and m.num_faltas < (d.carga_horaria/100)*25) ";
-  }
-  
-  if ($situacao == '2') {
-  	$sql .= " AND (m.nota_final < 60 or m.num_faltas > (d.carga_horaria/100)*25) ";
-  }
-
-  $sql .= " ORDER BY 1, 2";
-  
-  $sql = 'SELECT * FROM ('. $sql .') AS T1 ORDER BY lower(to_ascii("Nome"));';
-	
-
-  $RsCurso = $conn->Execute("SELECT descricao ||' (' || id || ') ' as \"Curso\" FROM cursos WHERE id = $curso;");
-  $info = "<strong>" . $RsCurso->fields[0] . "</strong><br />";	
-
-  $RsPeriodo = $conn->Execute("SELECT descricao FROM periodos WHERE id = '$periodo';");
-  $DescricaoPeriodo = $RsPeriodo->fields[0];
-  
-  $Result1 = $conn->Execute($sql);
-    
-  $num_result = $Result1->RecordCount();
-  
-  $info .= "Data: " . date("d/m/Y") . "&nbsp;&nbsp;-&nbsp;&nbsp;";
-  $info .= "Hora: " . date("H:i:s") . "&nbsp;&nbsp;-&nbsp;&nbsp;";
-  $info .= "Total de Registros: " . $num_result . "&nbsp;&nbsp;-&nbsp;&nbsp;";
-  $info .= "Período: $DescricaoPeriodo <br><br>";
-  
- //html2pdf
-  ob_start();  
 
 ?>
 <link href="../../../public/styles/style.css" rel="stylesheet" type="text/css">
@@ -82,31 +21,49 @@
 	<page_footer>	
 		<table border="0" cellpadding="0" cellspacing="0" style="width: 100%;">
   			<tr>
-    			<td style="text-align: left; width: 50%">&nbsp;</td>
-    			<td style="text-align: right; width: 50%">página [[page_cu]]/[[page_nb]]</td>
+    			<td style="text-align: right; width: 100%">
+    				página [[page_cu]]/[[page_nb]]
+    			</td>
   			</tr>
 		</table>
 	</page_footer>
-	<?php 
-		/**
-		 * FIXME: Inserir cabecalho e carimbo
-		 */
-	
-		//header
-	?>
-	<h2 style="font-size:16px;">RELAT&Oacute;RIO DE SITUA&Ccedil;&Atilde;O DE APROVA&Ccedil;&Atilde;O DE ALUNO(S)</h2>
+	<table border="0" cellpadding="0" cellspacing="0">
+  		<tr>
+    		<td style="text-align:center; width: 100%;">
+    			<?php echo $cabecalho;  ?>
+    		</td>
+		</tr>
+	</table>
+    <h2 style="font-size:16px;">
+    	RELAT&Oacute;RIO DE SITUA&Ccedil;&Atilde;O DE APROVA&Ccedil;&Atilde;O DE ALUNO(S)
+    </h2>
 	<?php	
   		echo $info;  
   		rs2html($Result1, 'width="90%" cellspacing="0" border="0" class="tabela_relatorio" cellpadding="0"'); 
 	?>
-	<br>
-	<br>
-	<?php //carimbo ?>
+	<br /><br />
+	<table border="0">
+  		<tr>
+    		<td width="200" style="text-align:center;">
+				<span class="carimbo_box">
+           		_______________________________
+           		<br>
+				<span class="carimbo_nome">
+		   			<?php echo $carimbo->get_nome($_POST['carimbo']);?>
+				</span>
+				<br />
+				<span class="carimbo_funcao">
+		   			<?php echo $carimbo->get_funcao($_POST['carimbo']);?>
+				</span>
+			</span>
+			</td>
+  		</tr>
+	</table>
 </page>
 <?php
-  	$content = ob_get_clean();
-  	require_once('../../lib/html2pdf/html2pdf.class.php');
-  	$pdf = new HTML2PDF('P','A4','en');
-  	$pdf->WriteHTML($content, isset($_GET['vuehtml']));
-  	$pdf->Output(); 
+	$content = ob_get_clean();
+	require_once('../../../lib/html2pdf/html2pdf.class.php');
+	$pdf = new HTML2PDF('P','A4','en');
+	$pdf->WriteHTML($content, isset($_GET['vuehtml']));
+	$pdf->Output(); 
 ?>
