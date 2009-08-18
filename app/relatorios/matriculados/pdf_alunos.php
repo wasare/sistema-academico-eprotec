6873cf21e84cb212c50ec84234c450226c2218f7
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-08-18 19:07:33 +0000

    Relatorio de alunos matriculados; CSS e classe sql2Excel.

--- app/relatorios/matriculados/pdf_alunos.php
+++ app/relatorios/matriculados/pdf_alunos.php
@@ -1,212 +1,54 @@
 <?php
 
-  //ARQUIVO DE CONFIGURACAO E CLASSE ADODB
-  header ("Cache-Control: no-cache");
-  require("../../lib/common.php");
-  require("../../configs/configuracao.php");
-  require("../../lib/adodb/adodb.inc.php");
-  require("../../lib/adodb/tohtml.inc.php");
-
-
-  //RECEBENDO OS DADOS DO FORMULARIO --
-  $periodo = $_POST["periodo1"];
-  $aluno = $_POST["aluno"];
-  $curso = $_POST["codigo_curso"];
-  $resp_nome = $_POST["resp_nome"];
-  $resp_cargo = $_POST["resp_cargo"];
-  $turma = $_POST["turma"];
-
-
-
-
-
-  //MONTANDO A SQL PARA A CONSULTA --
-  
-  $sql = " SELECT DISTINCT p.id as \"Código\""; 
-  
-  if (isset($_POST["nome"])) $sql .= ', p.nome as "Nome" '; 
-
-  if (isset($_POST["turma2"])) { 
-  
-	  $sql .= ', t.turma as "Turma" '; 
-	  $condicao_turma = "AND t.ref_curso = c.ref_curso AND t.ref_pessoa = p.id";
-	  $tabela_contrato = ", public.contratos t";
-  }
-  
-  if ($turma != '') { 
-  	
-	$condicao_turma .= " AND t.turma = '$turma' ";
-	$tabela_contrato = ", public.contratos t ";
-  }
-
-
-  //Dados de Filiacao
-  if (isset($_POST["pai"])) { 
-  
-	  $sql .= ', f.pai_nome as "Pai"'; 
-	  //$condicao_filiacao = " p.ref_filiacao = f.id AND ";
-	  
-	  $tabela_filiacao = "LEFT OUTER JOIN filiacao f ON(p.ref_filiacao = f.id)";
-	  //$tabela_filiacao = ", public.filiacao f ";
-  }
-  	
-  if (isset($_POST["mae"])) {
-  
-	  $sql .= ', f.mae_nome as "Mae" '; 
-	  //$condicao_filiacao = " p.ref_filiacao = f.id AND ";
-	  
-	  $tabela_filiacao = "LEFT OUTER JOIN filiacao f ON(p.ref_filiacao = f.id)";
-	  //$tabela_filiacao = ", public.filiacao f ";
-  }
-		
-  if (isset($_POST["endereco"])) {
-  	$sql .= ', p.rua || \' \' || p.complemento as "Endereço"'; 
-  }
-  
-  if (isset($_POST["bairro"])) {
-  	$sql .= ', p.bairro as "Bairro"'; 
-  }
-
-  //Dados de Cidade
-  if (isset($_POST["cidade"])) {
-	  
-	  $sql .= ', m.nome || \'-\' || m.ref_estado as "Cidade"'; 
-	  $condicao_municipio = " p.ref_cidade = m.id AND ";
-	  $tabela_municipio = ", public.aux_cidades m";
-  
-  }
-
-  if (isset($_POST["cep"])) {
-  	$sql .= ', p.cep as "CEP"'; 
-  }
-
-  if (isset($_POST["telefone"])) {
-	
-	  $sql .= ', p.fone_particular as "Tel. Part."
-		, p.fone_profissional as "Tel. Prof."
-		, p.fone_celular as "Tel. Cel."
-		, p.fone_recado as "Tel. Rec."
-	  '; 
-  
-  }
-	
-  if (isset($_POST["rg"])) $sql .= ', p.rg_numero as "RG"'; 
-	
-  if (isset($_POST["cpf"])) $sql .= ', p.cod_cpf_cgc as "CPF"'; 
-	
-  if (isset($_POST["sexo"])) $sql .= ', p.sexo as "Sexo"'; 	
-	
-  if (isset($_POST["data_nascimento"])) $sql .= ', to_char(p.dt_nascimento, \'DD/MM/YYYY\') as "Data de Nascimento"'; 
-
-  $sql .= " 
-  
-  FROM 
-  pessoas p $tabela_filiacao, matricula c $tabela_contrato $tabela_municipio 
-  WHERE 
-  c.ref_periodo = '$periodo' AND ";
-	
-  if ($curso != '') $sql .= " c.ref_curso = '$curso' AND"; 
-  
-  //$sql .= $condicao_filiacao; 
-  $sql .= $condicao_municipio;
-		
-  if ($aluno != '')  $sql .= " p.id = '$aluno' AND ";
-	
-  $sql .= " 
-  c.ref_pessoa = p.id  
-  $condicao_turma 
-  
-  ORDER BY 2";
-  
-  $sql = 'SELECT * FROM ('. $sql .') AS T1 ORDER BY lower(to_ascii("Nome"));';
-  //echo $sql;
-  //die();
-
-
-
-
-  
-  //Criando a classe de conexão ADODB
-  $Conexao = NewADOConnection("postgres");
-
-  //Setando como conexão persistente
-  $Conexao->PConnect("host=$host dbname=$database user=$user password=$password");
-
-  
-  $RsCurso = $Conexao->Execute("SELECT descricao ||' (' || id || ') ' as \"Curso\" FROM cursos WHERE id = $curso;");
-  $info = "<h4>" . $RsCurso->fields[0] . "</h4>";
-  
-  //Exibindo a descricao do periodo
-  $RsPeriodo = $Conexao->Execute("SELECT descricao FROM periodos WHERE id = '$periodo';");
-  $DescricaoPeriodo = $RsPeriodo->fields[0];
-  
-  
-  //EXECUTANDO SQL DA CONSULTA PRINCIPAL
-  $Result1 = $Conexao->Execute($sql);
-    
-  //numero de ocorrencias
-  $num_result = $Result1->RecordCount();
-  
-  
-  
-  //Informacoes de cabecalho
-  $info .= "<strong>Data: </strong>" . date("d/m/Y") . "&nbsp;&nbsp;-&nbsp;&nbsp;";
-  $info .= "<strong>Hora: </strong>" . date("H:i:s") . "&nbsp;&nbsp;-&nbsp;&nbsp;";
-  $info .= "<strong>Total de Registros: </strong>" . $num_result . "&nbsp;&nbsp;-&nbsp;&nbsp;";
-  $info .= "<strong>Período: </strong> <span>$DescricaoPeriodo</span> <br><br>";
-  
-  //Dados de rodape com assinatura
-  $rodape = '<span style="font-size: 12px;">' . $resp_nome . "</span><br>";
-  $rodape .= '<span style="font-size: 9px;"><strong>' . $resp_cargo . "</strong></span><br>";
-
-
- //html2pdf
-  ob_start();
+require_once('matriculados.php');
+ob_start();
 
 ?>
-<link href="../../public/styles/style.css" rel="stylesheet" type="text/css">
+<link href="../../../public/styles/style.css" rel="stylesheet" type="text/css">
 <page backtop="10mm" backbottom="10mm" >
-<page_header></page_header>
-<page_footer>
-<table style="width: 700px;">
-  <tr>
-    <td style="text-align: left; width: 50%">&nbsp;</td>
-    <td style="text-align: right; width: 50%">página [[page_cu]]/[[page_nb]]</td>
-  </tr>
-</table>
-</page_footer>
-<span style="text-align:center; font-size:12px;">
-	<img src="../../public/images/armasbra.jpg" width="57" height="60"><br />
-	MEC-SETEC<br />
-	CENTRO FEDERAL DE EDUCAÇÃO TECNOLÓGICA DE BAMBUÍ-MG<br />
-    SETOR DE REGISTROS ESCOLARES
-    <br /><br /><br />
-</span>
-<h2 style="font-size:16px;">RELAT&Oacute;RIO DE ALUNOS MATRICULADOS</h2>
-<?php
-  //Insere os dados de Cabecalho
-  echo $info;  
-  
-  //Gera a Tabela do Relatorio
-  rs2html($Result1, 'width="90%" cellspacing="0" border="0" class="tabela_relatorio" cellpadding="0"'); 
-?>
-<br>
-<br>
-<table width="90%" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td align="center">__________________________________________</td>
-  </tr>
-  <tr>
-    <td align="center"><?php echo $rodape; ?></td>
-  </tr>
-</table>
+	<page_header></page_header>
+	<page_footer>
+		<table style="width: 700px;">
+			<tr>
+				<td style="text-align: left; width: 50%">&nbsp;</td>
+				<td style="text-align: right; width: 50%">página [[page_cu]]/[[page_nb]]</td>
+			</tr>
+		</table>
+	</page_footer>
+	<table border="0" cellpadding="0" cellspacing="0">
+  		<tr>
+		<td style="text-align:center; width: 100%;">
+			<?php echo $header->get_empresa($PATH_IMAGES);  ?>
+		</td>
+		</tr>
+	</table>
+	<h2 style="font-size:16px;">RELAT&Oacute;RIO DE ALUNOS MATRICULADOS</h2>
+	<?php
+	  echo $info;  
+	  rs2html($Result1, 'width="90%" cellspacing="0" border="0" class="tabela_relatorio" cellpadding="0"'); 
+	?>
+	<br /><br />
+	<table border="0">
+  		<tr>
+    		<td width="200" style="text-align:center;">
+			<span class="carimbo_box">
+           		_______________________________<br />
+				<span class="carimbo_nome">
+		   			<?php echo $carimbo->get_nome($_POST['carimbo']);?>
+				</span>
+				<br />
+				<span class="carimbo_funcao">
+		   			<?php echo $carimbo->get_funcao($_POST['carimbo']);?>
+				</span>
+			</span>
+		</td>
+  		</tr>
+	</table>
 </page>
 <?php
-
-  	$content = ob_get_clean();
-  	require_once('../../lib/html2pdf/html2pdf.class.php');
-  	$pdf = new HTML2PDF('P','A4','en');
-  	$pdf->WriteHTML($content, isset($_GET['vuehtml']));
-  	$pdf->Output(); 
-
+$content = ob_get_clean();
+require_once('../../../lib/html2pdf/html2pdf.class.php');
+$pdf = new HTML2PDF('P','A4','en');
+$pdf->WriteHTML($content, isset($_GET['vuehtml']));
+$pdf->Output(); 
 ?>
--- app/relatorios/menu.php
+++ app/relatorios/menu.php
@@ -12,56 +12,70 @@ require("../../configs/configuracao.php");
 </head>
 
 <body>
-	<h2>Relat&oacute;rios</h2>
-
-	<input type="image" name="voltar" 
-		src="../../public/images/icons/back.png" 
-		alt="Voltar" 
-		title="Voltar" 
-		onclick="history.back(-1);return false;" class="botao" />
+<h2>Relat&oacute;rios</h2>
+<input type="image" name="voltar" 
+	src="../../public/images/icons/back.png" 
+	alt="Voltar" 
+	title="Voltar" 
+	id="bt_voltar" 
+	name="bt_voltar" 
+	class="botao" 
+	onclick="history.back(-1);return false;" />
 
 <div class="box_geral">
-	<a href="aprovados_reprovados/pesquisa_aprovados_reprovados.php" title="Alunos aprovados / reprovados" target="_self" class="menu_relatorio">
+	<div class="box_menu_relatorio">
+	<a href="aprovados_reprovados/pesquisa_aprovados_reprovados.php" title="Alunos aprovados/reprovados" target="_self">
 		<img src="../../public/images/icons/lupa.png" />&nbsp;Alunos Aprovados/Reprovados
-	</a><br />
-	<a href="dispensados/pesquisa_dispensados.php" title="Relat&oacute;rio de Alunos Dispensados" target="_self" class="menu_relatorio">
+	</a>
+	<br />
+	<a href="dispensados/pesquisa_dispensados.php" title="Alunos Dispensados" target="_self">
 		<img src="../../public/images/icons/lupa.png" />&nbsp;Alunos Dispensados
-	</a><br />
-	<a href="pesquisa_alunos.php" title="Alunos matriculados" target="_self" class="menu_relatorio">
-		<img src="../../public/images/icons/lupa.png" />&nbsp;Alunos Matriculados
-	</a><br />
-	<a href="pesquisa_todos_alunos_periodo.php"	title="Todos os alunos matriculado no per&iacute;odo" target="_self" class="menu_relatorio">
-		<img src="../../public/images/icons/lupa.png" />&nbsp;Alunos Matriculados (Todos)
-	</a><br />		
-	<a href="pesquisa_matriculados_por_cidade.php" title="Alunos matriculados por cidade de origem" target="_self" class="menu_relatorio">
+	</a>
+	<br />
+	<a href="matriculados/pesquisa_alunos.php" title="Alunos matriculados" target="_self">
+		<img src="../../public/images/icons/lupa.png" />&nbsp;Alunos Matriculados por curso
+	</a>
+	<br />		
+	<a href="pesquisa_matriculados_por_cidade.php" title="Alunos matriculados por cidade de origem" target="_self">
 		<img src="../../public/images/icons/lupa.png" />&nbsp;Alunos Matriculados por curso e cidade
-	</a><br />
-	<a href="pesquisa_alunos_novatos.php" title="Listagem de Alunos Novatos (v&iacute;nculo inicial no curso  / per&iacute;odo"	target="_self" class="menu_relatorio">
+	</a>
+	<br />
+	<a href="pesquisa_alunos_novatos.php" title="Listagem de Alunos Novatos (v&iacute;nculo inicial no curso / per&iacute;odo" target="_self">
 		<img src="../../public/images/icons/lupa.png" />&nbsp;Alunos Novatos
-	</a><br />
-	<a href="pesquisa_boletim.php"	title="Emiss&atilde;o dos Boletins Escolares" target="_self" class="menu_relatorio">
+	</a>
+	<br />
+	<a href="pesquisa_boletim.php"	title="Emiss&atilde;o dos Boletins Escolares" target="_self">
 		<img src="../../public/images/icons/lupa.png" />&nbsp;Boletim Escolar
-	</a><br />
-	<a href="pesquisa_cursos_no_periodo.php" title="Cursos em andamento" target="_self" class="menu_relatorio">
+	</a>
+	<br />
+	<a href="pesquisa_cursos_no_periodo.php" title="Cursos em andamento" target="_self">
 		<img src="../../public/images/icons/lupa.png" />&nbsp;Cursos em andamento
-	</a><br />
-	<a href="pesquisa_diarios.php" title="Andamento dos di&aacute;rio no per&iacute;odo" target="_self"	class="menu_relatorio">
-		<img src="../../public/images/icons/lupa.png" />&nbsp;Di&aacute;rios (andamento no per&iacute;odo)
-	</a><br />
-	<a href="egressos/pesquisa_egressos.php" title="Relat&oacute;rio de Egressos" target="_self" class="menu_relatorio">
-		<img src="../../public/images/icons/lupa.png" />&nbsp;Egressos
-	</a><br />
-	<a href="pesquisa_faltas_global.php" title="Relat&oacute;rio de Faltas Global por Per&iacute;odo / Curso" target="_self" class="menu_relatorio">
-		<img src="../../public/images/icons/lupa.png" />&nbsp;Faltas Global
 	</a>
 	<br />
-	<a href="pesquisa_ficha_academica.php" title="Vida acad&ecirc;mica do aluno por curso" target="_self" class="menu_relatorio">
-		<img src="../../public/images/icons/lupa.png" />&nbsp;Ficha Acad&ecirc;mica
-	</a>
-	<br />
-	<a href="declaracao_matricula/pesquisa_declaracao_matricula.php" title="Declara&ccedil;&atilde;o de matr&iacute;cula" target="_self" class="menu_relatorio">
+	<a href="declaracao_matricula/pesquisa_declaracao_matricula.php" title="Declara&ccedil;&atilde;o de matr&iacute;cula" target="_self">
 		<img src="../../public/images/icons/lupa.png" />&nbsp;Declara&ccedil;&atilde;o de matr&iacute;cula
 	</a>
+	<br />
+	<a href="pesquisa_diarios.php" title="Di&aacute;rios andamento no per&iacute;odo" target="_self">
+		<img src="../../public/images/icons/lupa.png" />&nbsp;Di&aacute;rios andamento no per&iacute;odo
+	</a>
+	<br />
+	<a href="egressos/pesquisa_egressos.php" title="Egressos" target="_self">
+		<img src="../../public/images/icons/lupa.png" />&nbsp;Egressos
+	</a>
+	<br />
+	<a href="pesquisa_faltas_global.php" title="Faltas global por per&iacute;odo / Curso" target="_self">
+		<img src="../../public/images/icons/lupa.png" />&nbsp;Faltas global
+	</a>
+	<br />
+	<a href="pesquisa_ficha_academica.php" title="Ficha acad&ecirc;mica" target="_self">
+		<img src="../../public/images/icons/lupa.png" />&nbsp;Ficha acad&ecirc;mica
+	</a>
+	<br />
+	<a href="pesquisa_todos_alunos_periodo.php" title="Pessoas com matr&iacute;cula" target="_self">
+		<img src="../../public/images/icons/lupa.png" />&nbsp;Pessoas com matr&iacute;cula
+	</a>
+	</div>
 </div>
 </body>
 </html>
