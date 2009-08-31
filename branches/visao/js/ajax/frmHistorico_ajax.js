861a5c9670bd9906f01aed322a14f113700eeeb7
wanderson <wanderson@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-08-31 19:59:47 +0000

    Retorna para revisão 113, excluindo arquivos importados por engano.

--- branches/visao/js/ajax/frmHistorico_ajax.js
+++
@@ -1,7 +0,0 @@
-//FUNÇÃO PARA SUBMETER FORMULÁRIO--------------------------------------------------
-function submitForm(nome, txtMetodo)
-{
-	$('txtMetodo').value = txtMetodo;
-	$('txtCodigo').value = txtCodigo;
-	$(nome).submit();
-}
--- branches/visao/js/ajax/frmRefeicao_ajax.js
+++
@@ -1,7 +0,0 @@
-//FUNÇÃO PARA SUBMETER FORMULÁRIO--------------------------------------------------
-function submitForm(nome, txtMetodo,txtCodigo, txtNome)
-{
-	$('txtMetodo').value = txtMetodo;
-	$('txtCodigo').value = txtCodigo;
-	$(nome).submit();
-}
