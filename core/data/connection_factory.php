8f04e57a319ea4a48ff308202c9358f9c8eef654
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-09-15 19:03:10 +0000

    Desenvolvimento da autenticacao do usuario e tela de login do usuario.

--- core/data/connection_factory.php
+++ core/data/connection_factory.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__).'/../../lib/adodb5/adodb.inc.php');
  * Connection factory
  * usando a bibliote ADODB
  */
-class connection_factory{
+class connection_factory {
 
     private $host;
     private $database;
@@ -12,8 +12,8 @@ class connection_factory{
     private $password;
     private $port;
     private $conn_persistent; //verifica se e conexao persistente
-	private $debug;
-	
+    private $debug;
+
     public $adodb; //Objeto de ADODB
 
     /**
@@ -21,61 +21,61 @@ class connection_factory{
      * @param vetor com parametros de configuracao
      * @param default true para conexao persistente
      */
-    public function __construct($arr, $conn_persistent = TRUE, $debug = FALSE){
+    public function __construct($arr, $conn_persistent = TRUE, $debug = FALSE) {
         $this->host 	= $arr['host'];
-		$this->user 	= $arr['user'];
-		$this->password = $arr['password'];
-		$this->database = $arr['database'];
+        $this->user 	= $arr['user'];
+        $this->password = $arr['password'];
+        $this->database = $arr['database'];
         $this->port 	= $arr['port'];
 
         $this->conn_persistent = $conn_persistent;
 
-		$this->debug = $debug;
+        $this->debug = $debug;
         $this->Open();
     }
 
     /**
      * Abre a conexao com o banco de dados
      */
-    public function Open(){
-	
-		$this->adodb = $GLOBALS['ADODB_SESS_CONN'];
+    public function Open() {
 
-		// reaproveita a conexao da sessao, caso exista uma e seja identica a conexao sendo criada
-		if(is_object($this->adodb) && $this->adodb->host == $this->host && $this->adodb->database == $this->database && $this->adodb->user == $this->user && $this->adodb->password == $this->passowrd) {
-			ADOdb_session::Persist($connectMode = $this->conn_persistent);
-		}
-		else {
+        $this->adodb = $GLOBALS['ADODB_SESS_CONN'];
 
-			$conn_data = "host=$this->host dbname=$this->database user=$this->user password=$this->password";
-	
-			$this->adodb = ADONewConnection("postgres");
-        
-			if($this->conn_persistent){
-				// Conexao persistente
-				if(!$this->adodb->PConnect($conn_data)) {
-					die('<h2 style="color: red">DB: Erro ao conectar com o banco de dados</h2>'.
-							'<div style="background-color: #ffffcc; padding:12px; margin:12px; font-size: 10px; width: 70%;">'.
-							$this->adodb->ErrorMsg() .'</div>');
-				}
-			} 
-			else {
-				// Conexao nao persistente
-				if(!$this->adodb->Connect($conn_data)) {
-						die('<h2 style="color: red">DB: Erro ao conectar com o banco de dados</h2>'.
-								'<div style="background-color: #ffffcc; padding:12px; margin:12px; font-size: 10px; width: 70%;">'.
-									$this->adodb->ErrorMsg() .'</div>');
-				}
-			}
-		}
+        // reaproveita a conexao da sessao, caso exista uma e seja identica a conexao sendo criada
+        if(is_object($this->adodb) && $this->adodb->host == $this->host && $this->adodb->database == $this->database && $this->adodb->user == $this->user && $this->adodb->password == $this->passowrd) {
+            ADOdb_session::Persist($connectMode = $this->conn_persistent);
+        }
+        else {
 
-		$this->adodb->debug = $this->debug;
-	}
-    
+            $conn_data = "host=$this->host dbname=$this->database user=$this->user password=$this->password";
+
+            $this->adodb = ADONewConnection("postgres");
+
+            if($this->conn_persistent) {
+            // Conexao persistente
+                if(!$this->adodb->PConnect($conn_data)) {
+                    die('<h2 style="color: red">DB: Erro ao conectar com o banco de dados</h2>'.
+                        '<div style="background-color: #ffffcc; padding:12px; margin:12px; font-size: 10px; width: 70%;">'.
+                        $this->adodb->ErrorMsg() .'</div>');
+                }
+            }
+            else {
+            // Conexao nao persistente
+                if(!$this->adodb->Connect($conn_data)) {
+                    die('<h2 style="color: red">DB: Erro ao conectar com o banco de dados</h2>'.
+                        '<div style="background-color: #ffffcc; padding:12px; margin:12px; font-size: 10px; width: 70%;">'.
+                        $this->adodb->ErrorMsg() .'</div>');
+                }
+            }
+        }
+
+        $this->adodb->debug = $this->debug;
+    }
+
     /**
      * Fecha conexao
      */
-    public function Close(){
+    public function Close() {
         $this->adodb->Close();
     }
 
@@ -84,12 +84,12 @@ class connection_factory{
      * @param sql
      * @return ResultSet
      */
-    public function Execute($sql){
-		if (!$ResultSet = $this->adodb->Execute($sql)) {
-				die('<h2 style="color: red">DB: Erro ao executar consulta</h2>'.
-					'<div style="background-color: #ffffcc; padding:12px; margin:12px; font-size: 10px; width: 70%;">'.
-            	  		$this->adodb->ErrorMsg() .'</div>');
-		}
+    public function Execute($sql) {
+        if (!$ResultSet = $this->adodb->Execute($sql)) {
+            die('<h2 style="color: red">DB: Erro ao executar consulta</h2>'.
+                '<div style="background-color: #ffffcc; padding:12px; margin:12px; font-size: 10px; width: 70%;">'.
+                $this->adodb->ErrorMsg() .'</div>');
+        }
         return $ResultSet;
     }
 }
--- core/login/auth.php
+++ core/login/auth.php
@@ -4,98 +4,167 @@
  * @filesource
  * @copyright 2009 IFMG Campus Bambui
  * @author santiago
+ * @author wanderson
  * @version 1
- * @package sistema-academico
- * @subpackage sistema-academico.core.login
+ * @since 2009-09-01
+ * @package sa
+ * @subpackage sa.core.login
  */
 class auth {
 
+/**
+ * Efetua a autenticação do usuário em um módulo do SA
+ * @param Login
+ * @param Senha
+ * @param Módulo que vai acessar no SA
+ * @param conexao com banco de dados
+ * @return efetuado ou rejeitado
+ */
+    public function login($login, $senha, $modulo, $conn) {
 
-    public function __construct() {
-    //
-    }
+        $log_msg = $_SERVER['REMOTE_ADDR'] .' - ['. date("d/m/Y H:i:s") .'] - ';
 
-    /**
-     * Autentica usuario no SA
-     * @param usuario
-     * @param senha
-     * @return Sessao de usuario logado
-     */
-    public function login($auth_user, $auth_pwd) {
-
-        $logado = '';//recebe variavel de sessao do usuario
-
-        if(empty($logado)) {
-
-            if(empty($auth_user) OR empty($auth_pwd)) {
-                return 'Usuário e/ou senha não preenchido!';
-            }else {
-
-                require_once('../../configs/configuracao.php');
-                require_once('../data/connection_factory.php');
-
-                $conn = new connection_factory($param_conn);
-
-                $sql = "SELECT COUNT(*) 
-                        FROM usuario
-                        WHERE
-                            nome = '$auth_user' AND
-                            senha = '$auth_pwd'; ";
-                
-                //retorna um unico valor - uma variavel simples
-                if($conn->adodb->getOne($sql) == 1) {
-
-                    $this->set_user($auth_user);
-                    
-                    //redirecionar para a pagina inicio.php
-                    
-                    return 'Usuário '.$auth_user.' autenticado...';
-
-                }else {
-                    return 'Usuário e/ou senha incorreto!';
-                }
-            }
+        //Verifica o usuario no banco
+        if(empty($login) OR empty($senha)) {
+            $verifica_usuario = false;
         }else {
-            return 'conectado';
+
+            $sql = "SELECT COUNT(*) FROM usuario
+                    WHERE nome = '$login' AND senha = '$senha'; ";
+
+            //retorna o primeiro valor da consulta
+            if($conn->adodb->getOne($sql) == 1) {
+                $verifica_usuario = true;
+            }else {
+                $verifica_usuario = false;
+            }
+        }
+
+        if($verifica_usuario) {
+
+            //Grava log com sucesso de login
+            $log_msg .= $login .' - *** LOGIN ACEITO (host='.
+                $param_conn['host'] .',db='.
+                $param_conn['database'] .',uid='.
+                $login .',pwd=) ***'."\n";
+
+            error_log($log_msg,3,$LOGIN_LOG_FILE);
+
+            $GLOBALS['USERID'] = trim($login);
+            $GLOBALS['ADODB_SESSION_EXPIRE_NOTIFY'] = array('USERID','session::clear_session');
+
+            $_SESSION['sa_auth'] = trim($login) .':'. trim($senha);
+            
+            $_SESSION['sa_modulo'] = $modulo;
+
+            switch ($modulo) {
+                case 'sa_login':
+                    header('Location: '. $BASE_URL .'app/index.php');
+                    break;
+                case 'web_diario_login':
+                    header('Location: '. $BASE_URL .'app/webdiario/');
+                    break;
+                case 'aluno_login':
+                    header('Location: '. $BASE_URL .'app/aluno');
+                    break;
+            }
+        }
+        else {
+            //Grava log com erro de login
+            $log_msg .=  $login .' - *** LOGIN RECUSADO (host='.
+                $param_conn['host'] .',db='.
+                $param_conn['database'] .',uid='.
+                $login .',pwd=) ***'."\n";
+
+            error_log($log_msg,3,$LOGIN_LOG_FILE);
+
+            // TODO: retornar o erro de tentativa de login na proprio formulario de login
+            //Erro: senha ou usuário inválido
+            if ($modulo == 'sa_login') {
+                header('Location: '. $BASE_URL .'index.php?sa_msg=1');
+            }
+            if ($modulo == 'web_diario_login') {
+                header('Location: '. $BASE_URL .'index.php?sa_msg=2');
+            }
+            if ($modulo == 'aluno_login') {
+                header('Location: '. $BASE_URL .'index.php?sa_msg=3');
+            }
         }
     }
 
+
     /**
-     * Destroi a autenticacao do usuario logado
+     * Checa a autenticação do usuário
      * @return void
      */
-    public function logout() {
-    //destroi a variavel de sessao do usuario
+    public function check_login($BASE_URL, $SESS_TABLE, $LOGIN_LOG_FILE) {
+
+        //require_once(dirname(__FILE__).'/../../config/configuracao.php');
+        
+        var_dump($_SESSION['sa_auth']);
+        //echo ' caminho: '.$BASE_URL . $SESS_TABLE . $LOGIN_LOG_FILE;
+        die;
+
+        // TODO: registrar em log quando falhar a verificacao - gravar em banco de dados
+
+        $log_msg = $_SERVER['REMOTE_ADDR'] .' - ['. date("d/m/Y H:i:s") .'] - ';
+
+
+        if(empty($_SESSION['sa_auth'])) {
+            //FIXME: if verificando o modulo para voltar para a pagina inicial, pode ser aluno
+            header('Location: '. $BASE_URL .'index.php?sa_msg=4');
+            exit;
+        }else {
+            
+            /* FIXME: Verificar variavel de sessao $_SESSION['sa_auth'] se esta vazia ou exite
+               executar uma nova consulta para validar os dados de autenticacao */
+
+            list($uid, $pwd) = explode(":",$_SESSION['sa_auth']);
+
+            // verifica e desconecta usuario com duas sessoes simultaneas
+            $cont_sess = $GLOBALS['ADODB_SESS_CONN']->getOne("SELECT COUNT(*) FROM $SESS_TABLE WHERE expireref = '". $uid ."';");
+
+            if($cont_sess > 1) {
+            // exclui a sessao do usuario no banco de dados
+                session::clear_session($uid, NULL);
+                session::destroy();
+
+                // grava log de falha de acesso
+                $log_msg .= $param_conn['user'] .' - *** LOGIN DUPLICADO (host='.
+                    $param_conn['host'] .',db='.
+                    $param_conn['database'] .',uid='.
+                    $param_conn['user'] .',pwd=) ***'."\n";
+
+                error_log( $log_msg,3,$LOGIN_LOG_FILE);
+
+                // TODO: redirecionar a uma pagina de aviso de sessao expirada por duplicidade
+                header('Location: '. $BASE_URL .'index.php?sa_msg=5');
+                exit;
+            }
+
+            //Criar flag Se validado para substituir o se abaixo
+            if(empty($uid) && empty($pwd)) {
+
+                unset($_SESSION['sa_auth']);
+
+                // grava log de falha de acesso
+                $log_msg .= $param_conn['user'] .' - *** FALHA AO VERIFICAR LOGIN (host='.
+                    $param_conn['host'] .',db='.
+                    $param_conn['database'] .',uid='.
+                    $param_conn['user'] .',pwd=) ***'."\n";
+
+                error_log( $log_msg,3,$LOGIN_LOG_FILE);
+
+                header('Location: '. $BASE_URL .'index.php?sa_msg=');
+                exit;
+            }
+            else {
+                $GLOBALS['USERID'] = $uid;
+                $GLOBALS['ADODB_SESSION_EXPIRE_NOTIFY'] = array('USERID','session::clear_session');
+            }
+        }
     }
 
-    public function set_user($auth_user) {
-    //cria a variavel de sessao do usuario
-    }
-
-    public function get_user() {
-    //busca a variavel de sessao do usuario logado
-    }
-
-    /*
-    public function save($user){
-        //insere e altera usuario
-    }
-    
-    public function remove($id){
-        //remove usuario
-    }
-    
-    public function fetch_all(){
-        //exibe todos os usuarios
-    }
-    
-    public function find($user){
-        //pesquisa usuario
-    }
-    */
 }
-
-$au = new auth();
-echo $au->login('teste','123');
 
 ?>
