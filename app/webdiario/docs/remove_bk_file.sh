d6cd475ec529290f508f5f014b6c58d5eb246282
wanderson <wasare@users.noreply.github.com>
2009-10-02 11:10:24 +0000

    - integra módulo do web diário da secretaria ao SA, refs #37

--- app/webdiario/docs/remove_bk_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/bin/find ./ -iname "*~" -exec echo {} \;
-/usr/bin/find ./ -iname "*~" -exec rm {} \;
-
-/usr/bin/find ./ -iname "caderno_chamada_*.ps" -exec echo {} \;
-/usr/bin/find ./ -iname "caderno_chamada_*.ps" -exec rm {} \;
-
