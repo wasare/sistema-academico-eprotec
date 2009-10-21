4b255d05757d9d0aca5d66196ccc6089c696a921
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-10-21 16:33:55 +0000

    Bug no css do menu principal. Bug no help/docs.

--- public/help.php
+++ public/help.php
@@ -1,7 +1,6 @@
 <?php
 
-header("Cache-Control: no-cache");
-require_once("../core/login/check_login.php");
+//require_once("../core/login/check_login.php");
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
