4ba344e1a2c43422094a7d30043d18168c91c4ea
wanderson <wanderson@a1917718-5e30-f394-388a-3cf0e972ec9c>
2009-12-14 18:29:51 +0000

    - alterações diversas para a nova versão do web diário, refs #228

---
+++ trunk/app/web_diario/movimentos/chamada/exclui_chamada.php
@@ -0,0 +1,151 @@
+<?php
+
+require_once(dirname(__FILE__) .'/../../../setup.php');
+require_once($BASE_DIR .'core/web_diario.php');
+require_once($BASE_DIR .'core/date.php');
+
+$conn = new connection_factory($param_conn);
+
+$diario_id = (int) $_GET['id'];
+$operacao = $_GET['do'];
+
+$sa_ref_periodo = $_SESSION['web_diario_periodo_id'];
+
+/*
+TODO: verifica o direito de acesso do usuário ao diário informado
+*/
+
+if (is_finalizado($diario_id)){
+
+    echo '<script language="javascript" type="text/javascript">';
+    echo 'alert("ERRO! Este diário está finalizado e não pode ser alterado!");';
+    echo 'window.close();';
+    echo '</script>';
+    exit;
+}
+
+if(isset($_POST['exclui_ok']) && $_POST['exclui_ok'] === 'exclui_chamada') {
+
+	// EXCLUI TODAS AS FALTAS ANTERIORES PARA A CHAMADA
+
+	$sql_faltas = " SELECT id, ra_cnec
+                     FROM
+                        diario_chamadas a
+                    WHERE
+                        a.ref_disciplina_ofer = ". $_POST['diario_id'] ." AND
+                        a.data_chamada = '". $_POST['select_data_chamada'] ."';";
+
+	$faltas = $conn->get_all($sql_faltas);
+
+	if(count($faltas) > 0) {
+
+    	foreach( $faltas as $f )
+    	{
+        	$valor = $f['id'];
+        	$ref_pessoa = $f['ra_cnec'];
+
+        	// DELETA A FALTA DO DIARIO
+        	$sql1 = " BEGIN; DELETE FROM diario_chamadas WHERE id = $valor;";
+
+        	falta($ref_pessoa, $_POST['diario_id'], 1, 'SUB', $sql1);
+   		}
+	}
+
+
+	$sql1 = "DELETE 
+         FROM 
+            diario_seq_faltas 
+         WHERE  
+            ref_disciplina_ofer = ". $_POST['diario_id'] ." AND   
+            dia = '". $_POST['select_data_chamada'] ."';";
+
+	$conn->Execute($sql1);
+
+
+	echo '<script language="javascript" type="text/javascript">  window.alert(\'Foram excluídos com sucesso \n os registros referente ao dia ' . $_POST['select_data_chamada'] . '\'); javascript:window.history.back(1); </script>';
+	exit;
+
+}
+
+?>
+<head>
+<title><?=$IEnome?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="<?=$BASE_URL .'public/styles/web_diario.css'?>" type="text/css">
+
+<script language="javascript" type="text/javascript">
+function jsConfirm(dia)
+{
+   if (! dia == "")
+   {
+    if (! confirm('Você realmente deseja apagar \n a chamada do dia ' + dia + '?' + '\nTodas as faltas lançadas nesta data, \n caso existam, serão excluídas!'))
+      {
+         return false; 
+      } 
+      else 
+      {
+         document.getElementById('exclui_chamada').submit();
+         return true;
+      }
+   }
+   else return false;
+}
+
+</script>
+
+</head>
+
+<body>
+
+<div align="left" class="titulo">
+	  <h3>Exclus&atilde;o de Chamada</h3>
+</div>
+<p style="font-size:0.9em; font-face:Verdana, Arial, Helvetica, sans-serif; font-weight:bold; color:red;"> 
+        Este processo exclui a chamada do dia selecionado! <br /> O professor dever&aacute; refazer a chamada posteriormente, caso seja necess&aacute;rio.</p>
+<br />
+<?=papeleta_header($diario_id)?>
+<br />
+<?php
+
+ $sql4 = "SELECT DISTINCT
+                 id,
+                 id_prof,
+                 periodo,
+                 curso,
+                 disciplina,
+                 dia,
+                 ref_disciplina_ofer as idof
+                 FROM
+                 diario_seq_faltas
+                 WHERE
+                 ref_disciplina_ofer = $diario_id ORDER BY dia DESC; ";
+
+ $chamadas = $conn->get_all($sql4);
+?>
+
+
+<form name="exclui_chamada" id="exclui_chamada" action="" method="post">
+<input type="hidden" name="diario_id" id="diario_id" value="<?=$diario_id?>">
+<input type="hidden" name="operacao" id="operacao" value="<?=$operacao?>">
+<input type="hidden" name="exclui_ok" id="exclui_ok" value="<?=$operacao?>">
+
+
+<p>Selecione a data da chamada a excluir:</p>
+<select name="select_data_chamada" id="select_data_chamada" class="select" onchange="jsConfirm(this.value);">
+<option value="">--- data de chamada ---</option>
+
+<?php
+	
+	foreach($chamadas as $data) {
+
+		echo '<option value="'. date::convert_date($data['dia']) .'">'. date::convert_date($data['dia']) .'</option>';
+	}
+?>	
+	</select>
+
+&nbsp;&nbsp;ou&nbsp;
+<a href="#" onclick="javascript:window.close();">fechar</a>
+</form>
+</body>
+</head>
+</html>
---
+++ trunk/app/web_diario/requisita.php
@@ -0,0 +1,316 @@
+<?php
+
+require_once('../../app/setup.php');
+require_once($BASE_DIR .'core/web_diario.php');
+
+$conn = new connection_factory($param_conn);
+
+$diario_id = (int) $_GET['id'];
+$operacao = $_GET['do'];
+
+$_SESSION['web_diario_do'] = $operacao;
+
+if(!is_numeric($diario_id) && !is_numeric($operacao))
+{
+	echo '<script language="javascript">
+                window.alert("ERRO! Dados invalidos!");
+                window.close();
+    </script>';
+    exit; 
+}
+
+/*
+TODO: verifica o direito de acesso do usuário ao diário informado
+*/
+$_GET['diario_id'] = $diario_id;
+$_POST['diario_id'] = $diario_id;
+
+
+echo 'Meus di&aacute;rios  | Per&iacute;odos  | Coordena&ccedil;&atilde;o  | Ferramentas  | Sair <br /><br />';
+
+
+// OPERACOES COM ALTERACAO DE DADOS
+if($operacao == 'chamada') {
+    require_once($BASE_DIR .'app/web_diario/movimentos/chamada/chamadas.php');
+    exit;
+}
+
+if($operacao == 'notas') {
+    require_once($BASE_DIR .'app/web_diario/movimentos/notas/lanca_notas.php');
+    exit;
+}
+
+if($operacao == 'altera_chamada') {
+    require_once($BASE_DIR .'app/web_diario/movimentos/faltas/faltas.php');
+    exit;
+}
+
+
+if($operacao == 'exclui_chamada') {
+    require_once($BASE_DIR .'app/web_diario/movimentos/chamada/exclui_chamada.php');
+    exit;
+}
+
+if($operacao == 'marca_diario') {
+	echo papeleta_header($diario_id);
+    require_once($BASE_DIR .'app/web_diario/movimentos/marca_concluido.php');
+	echo '<br />';
+	echo '<script language="javascript" type="text/javascript"> 
+			alert(\'Diario marcado / desmarcado com sucesso!\');
+			window.opener.location.reload();
+			setTimeout("self.close()",450); </script>';
+    exit;
+}
+// ^ OPERACOES COM ALTERACAO DE DADOS   ^ //
+
+
+
+// RELATORIOS
+if($operacao == 'papeleta') {
+    require_once($BASE_DIR .'app/relatorios/web_diario/papeleta.php');
+    exit;
+}
+
+if($operacao == 'papeleta_completa') {
+    require_once($BASE_DIR .'app/relatorios/web_diario/papeleta_completa.php');
+    exit;
+}
+
+if($operacao == 'faltas_completo') {
+    require_once($BASE_DIR .'app/relatorios/web_diario/faltas_completo.php');
+    exit;
+}
+
+if($operacao == 'caderno_chamada') {
+    require_once($BASE_DIR .'app/relatorios/web_diario/caderno_chamada.php');
+    exit;
+}
+
+if($operacao == 'conteudo_aula') {
+    require_once($BASE_DIR .'app/relatorios/web_diario/conteudo_aula.php');
+    exit;
+}
+// ^ RELATORIOS ^ //
+
+echo '<script type="text/javascript">window.focus.();</script>';
+
+exit;
+/*
+
+*/
+$sql3 = "SELECT 
+         b.nome, b.id AS ra_cnec, a.ordem_chamada, a.nota_final, a.num_faltas 
+         FROM matricula a, pessoas b
+         WHERE 
+            (a.dt_cancelamento is null) AND
+            a.ref_disciplina_ofer = $diario_id AND
+            a.ref_pessoa = b.id AND 
+            a.ref_motivo_matricula = 0
+            
+         ORDER BY lower(to_ascii(nome));" ;
+
+
+$qry3 = $conn->get_all($sql3);
+
+$matriculas = count($qry3);
+
+$sql5 = " SELECT fl_digitada, fl_concluida
+            FROM
+                disciplinas_ofer
+            WHERE
+               id = $diario_id;";
+		   
+$qry5 = $conn->get_row($sql5);
+
+$fl_digitada = $qry5['fl_digitada'];
+$fl_concluida = $qry5['fl_concluida'];
+
+// APROVEITAMENTO DE ESTUDOS 2
+// CERTIFICACAO DE EXPERIENCIAS 3
+// EDUCACAO FISICA 4
+$msg_dispensa = '';
+
+$sql_dispensas = "SELECT COUNT(*) 
+         			FROM 
+						matricula a, pessoas b
+         			WHERE 
+            
+            		(a.dt_cancelamento is null) AND            
+            		a.ref_disciplina_ofer = $diario_id AND
+            		a.ref_pessoa = b.id AND 
+            		a.ref_motivo_matricula IN (2,3,4) ;" ;
+
+$dispensas = $conn->adodb->getOne($sql_dispensas);
+
+if ($dispensas > 0 ) {
+	if($dispensas == 1)
+        $msg_dispensa .= '<font size="-1" color="brown"><strong>*</strong> ' . $dispensas . ' aluno dispensado, consulte a papeleta completa para exib&iacute;-lo. </font>';
+	else
+		$msg_dispensa .= '<font size="-1" color="brown"><strong>*</strong> ' . $dispensas . ' alunos dispensados, consulte a papeleta completa para exib&iacute;-los. </font>';
+}
+
+
+?>
+
+<html>
+<head>
+<title><?=$IEnome?> - papeleta</title>
+
+<link rel="stylesheet" href="<?=$BASE_URL .'public/styles/web_diario.css'?>" type="text/css">
+
+<style media="print">
+<!--
+.nao_imprime {display:none}
+
+table.papeleta {
+    font: 0.7em verdana, arial, tahoma, sans-serif;
+    border: 0.0015em solid;
+    border-collapse: collapse;
+    border-spacing: 0px;
+}
+
+.papeleta td {
+    font: 0.7em verdana, arial, tahoma, sans-serif;
+    border: 0.0015em solid;
+    padding: 2px;
+    border-collapse: collapse;
+    border-spacing: 1px;
+}
+-->
+</style>
+
+</head>
+
+<body>
+<font size="2">
+
+<?php
+
+
+echo papeleta_header($diario_id);
+
+
+if($fl_digitada == 'f' && $fl_concluida == 'f') {
+        $fl_situacao = '<font color="green"><b>Aberto</b></font>';
+}
+else {
+	if($fl_concluida == 't') {
+            $fl_situacao = '<font color="blue"><b>Conclu&iacute;do</b></font>';
+    }
+
+    if($fl_digitada == 't') {
+        $fl_situacao = '<font color="red"><b>Finalizado</b></font>';
+        $fl_encerrado = 1;
+    }
+}
+
+echo 'Situa&ccedil;&atilde;o: ' . $fl_situacao;
+
+if( $fl_digitada == 'f') {
+
+    echo '<br /><font color="red" size="-2"><strong>SEM VALOR COMO DOCUMENTO, PASS&Iacute;VEL DE ALTERA&Ccedil;&Otilde;ES</strong></font>';
+
+}
+
+?>
+</font>
+<table cellspacing="0" cellpadding="0" class="papeleta">
+	<tr bgcolor="#cccccc">
+		<td  align="center"><b>N&ordm;</b></td>
+		<td  align="center"><b>Matr&iacute;cula</b></td>
+		<td><b>Nome</b></td>
+		<td align="center"><b>Nota</b></td>
+		<td align="center"><b>Falta</b></td>
+	</tr>
+
+<?php
+
+    
+$sql_carga_horaria = "SELECT get_carga_horaria_realizada($diario_id), get_carga_horaria(get_disciplina_de_disciplina_of($diario_id));"; 
+
+$carga_horaria = $conn->adodb->getRow($sql_carga_horaria);
+
+$ch_prevista = $carga_horaria['get_carga_horaria'];
+$ch_realizada = $carga_horaria['get_carga_horaria_realizada'];
+
+$FaltaMax = $ch_realizada * 0.25;
+
+$i = 0;
+$N = 1;
+
+$r1 = '#FFFFFF';
+$r2 = '#FFFFCC';
+
+
+foreach($qry3 as $row3) 
+{
+   $nome_f = $row3['nome'];
+   $racnec = $row3['ra_cnec'];
+   $racnec = str_pad($racnec, 5, "0", STR_PAD_LEFT) ;
+   $num = $row3['ordem_chamada'];
+   
+   if ($row3['num_faltas'] > 0){
+      $falta = $row3['num_faltas'];
+   }
+   else{
+      $falta = '0';
+   }
+
+   if($falta > $FaltaMax) $falta = "<font color=\"red\"><b>$falta</b></font>";
+   
+    if($row3['nota_final'] != 0) { 
+		$nota = number_format($row3['nota_final'],'1',',','.');
+	}
+	else { 
+		$nota = $row3['nota_final'];
+	}
+ 
+	 
+   if ($nota < 60) 
+   {
+      $nota = "<font color=\"red\"><b>$nota</b></font>";
+   }
+   
+   if ( ($i % 2) == 0)
+   {
+      $rcolor = $r1;
+   }
+   else
+   {
+      $rcolor = $r2;
+   }
+   print("<tr bgcolor=\"$rcolor\">\n"); 
+   print(" <td align=\"center\" >". $N++ ."</td>\n <td align=\"center\" >$racnec</td>\n <td>$nome_f</td>\n "); 
+   print ("<td align=\"center\">$nota</td>\n ");
+   print ("<td align=\"center\">$falta</td>\n ");
+   print("</tr>\n ");
+   
+   $i++;
+}
+
+?>
+
+
+</table>
+
+<?=$msg_dispensa?>
+
+<hr width="60%" size="1" align="left" color="#FFFFFF">
+
+<?php
+	
+print("Aulas dadas: <b>$ch_realizada</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+print("Aulas previstas: <b>$ch_prevista</b> <br />");
+print("<br />ASSINATURA(S):");
+
+
+?>
+<br /><br />
+<div class="nao_imprime">
+<input type="button" value="Imprimir" onClick="window.print()">
+&nbsp;&nbsp;&nbsp;
+<input type="button" name="fechar" id="fechar" value="Fechar" onclick="javascript:window.close();" />
+</div>
+<br /><br />
+</body>
+</html>
