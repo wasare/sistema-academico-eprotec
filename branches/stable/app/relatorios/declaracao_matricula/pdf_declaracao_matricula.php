140e4230816d5936f7a7d949cb6d980f9815a256
wanderson <wanderson@a1917718-5e30-f394-388a-3cf0e972ec9c>
2010-02-02 18:33:47 +0000

    - adequar o leiaute da declaração de matrícula, refs #273

--- branches/stable/app/relatorios/declaracao_matricula/pdf_declaracao_matricula.php
+++ branches/stable/app/relatorios/declaracao_matricula/pdf_declaracao_matricula.php
@@ -1,38 +1,37 @@
 <?php
 
-require_once("declaracao_matricula.inc.php");
+require_once('declaracao_matricula.inc.php');
 require_once('../../../lib/fpdf153/fpdf.php');
 
 class PDF extends FPDF
 {
-
-
 }
 
 $pdf=new PDF();
 
 $pdf->AddPage();
 
-$pdf->SetMargins(30,20,20);
+$pdf->SetMargins(20,30,20);
 
 $pdf->SetTitle('Declaração de matrícula');
 
-$pdf->SetFont('Times','B',12);
+$pdf->SetFont('Times','B',14);
 //$pdf->Write(5,'DECLARAÇÃO');
-$pdf->Ln(50);
+$pdf->Ln(70);
 $pdf->Cell(0,5,'DECLARAÇÃO',0,1,'C',0,0,0);
-$pdf->Ln(8);
-$pdf->SetFont('Times','',12);
+$pdf->Ln(15);
+$pdf->SetFont('Times','',13);
 $pdf->MultiCell(0,5,$corpo,0,'J');
-$pdf->Ln(8);
+$pdf->Ln(10);
 $pdf->Cell(0,5,$data_declaracao,0,1,'C',0,0,0);
-$pdf->Ln(20);
-$pdf->Cell(0,5,$carimbo_nome,0,1,'C',0,0,0);
+$pdf->Ln(15);
+$pdf->Cell(44);
+$pdf->Cell(70,5,$carimbo_nome,0,1,'C',0,0,0);
 $pdf->SetFont('Times','',10);
 $pdf->Cell(44);
 $pdf->MultiCell(70,5,$carimbo_dados,0,'C');
 //$pdf->SetFont('Times','',12);
-$pdf->Ln(20);
+$pdf->Ln(15);
 $pdf->MultiCell(0,5,$decretos,0,'J');
 $pdf->Ln(6);
 //$pdf->SetDrawColor(0,0,0);
@@ -40,7 +39,7 @@ $pdf->Ln(6);
 //$pdf->Line(0,0,0,100);
 $pdf->Cell(0,0,'',1,0);
 $pdf->Ln(2);
-$pdf->SetFont('Times','',10);
+$pdf->SetFont('Times','',9);
 $pdf->MultiCell(0,5,$empresa,0,'C');
 
 $pdf->Output();
