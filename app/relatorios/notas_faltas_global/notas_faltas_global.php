19eb410ee2af811e3bba194a2f6523bce3cc0339
santiago <santiago@a1917718-5e30-f394-388a-3cf0e972ec9c>
2010-04-08 12:43:42 +0000

    Relatório de notas e faltas global com problemas. fecha #304

--- app/relatorios/notas_faltas_global/notas_faltas_global.php
+++ app/relatorios/notas_faltas_global/notas_faltas_global.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Arquivo com as configuracoes iniciais
- */
+*/
 require_once("../../../app/setup.php");
 require_once("../../../core/reports/header.php");
 
@@ -10,13 +10,14 @@ $header  = new header($param_conn);
 /**
  * Parametros do formulario
  */
-$periodo = $_POST['periodo'];;
+$periodo = $_POST['periodo'];
+;
 $campus  = $_POST['campus'];
 $curso   = $_POST['curso'];
 $turma   = $_POST['turma'];
 
 if(empty($periodo) or empty($campus) or empty($curso) or empty($turma) or
-    !isset($periodo) or !isset($campus) or !isset($curso) or !isset($turma) ) {
+        !isset($periodo) or !isset($campus) or !isset($curso) or !isset($turma) ) {
     echo '<script language="javascript">window.alert("Nenhum diario a ser exibido!");</script>';
     echo '<meta http-equiv="refresh" content="0;url=index.php">';
     exit;
@@ -24,7 +25,7 @@ if(empty($periodo) or empty($campus) or empty($curso) or empty($turma) or
 
 /*
  * Estancia a classe de conexao e abre
- */
+*/
 $conn = new connection_factory($param_conn);
 
 
@@ -59,29 +60,6 @@ $desc_curso = $conn->get_row($sql_curso);
 /**
  * Conteudo da legenda
  */
-/*$sql_legenda = "
-SELECT
-    o.id AS diario,
-    d.id,
-    d.descricao_disciplina,
-    d.descricao_extenso,
-    d.carga_horaria, 
-    p.id || ' - ' || p.nome AS prof,
-    o.fl_concluida,
-    o.fl_digitada
-FROM
-    disciplinas d, disciplinas_ofer o, disciplinas_ofer_prof dp, pessoas p
-WHERE
-    o.ref_periodo = '".$periodo."' AND
-    d.id = o.ref_disciplina AND
-    dp.ref_disciplina_ofer = o.id AND
-    dp.ref_professor IS NOT NULL AND
-    p.id = dp.ref_professor AND
-    o.turma = '".$turma."' AND
-    o.ref_curso = ".$curso." AND
-    is_cancelada = '0'
-ORDER BY diario;";*/
-
 $sql_legenda = "
 SELECT
     o.id AS diario,
@@ -89,47 +67,18 @@ SELECT
     d.descricao_disciplina,
     d.descricao_extenso,
     d.carga_horaria,
-    p.id || ' - ' || p.nome AS prof,
+    professor_disciplina_ofer_todos(o.id) AS prof,
     o.fl_concluida,
     o.fl_digitada
 FROM
-    disciplinas d, disciplinas_ofer o, disciplinas_ofer_prof dp, pessoas p
+    disciplinas d, disciplinas_ofer o, disciplinas_ofer_prof dp
 WHERE
-    o.ref_periodo = '".$periodo."' AND
     d.id = o.ref_disciplina AND
     dp.ref_disciplina_ofer = o.id AND
     dp.ref_professor IS NOT NULL AND
-    p.id = dp.ref_professor AND
-    o.id IN(
-            SELECT DISTINCT m.ref_disciplina_ofer
-            FROM contratos c, matricula m
-            WHERE
-                c.id = m.ref_contrato AND
-                c.ref_curso = $curso AND
-                c.turma = '$turma'
-        ) AND
-    o.ref_curso = ".$curso." AND
-    is_cancelada = '0'
-ORDER BY diario;";
-
-
-$arr_legenda = $conn->get_all($sql_legenda);
-
-/*if(count($arr_legenda) == 0) {
-    echo '<script language="javascript">window.alert("Nenhum diario encontrado!");</script>';
-    //echo '<meta http-equiv="refresh" content="0;url=index.php">';
-    echo '<script language="javascript">window.close();</script>';
-    exit;
-}
-*/
-
-/**
- * Consulta principal
- */
-/*$sql_rel = "
-SELECT * FROM (
-    SELECT DISTINCT
-        b.nome, b.id as matricula, a.nota_final, a.num_faltas, ref_disciplina_ofer
+    o.id IN (
+			SELECT DISTINCT
+        ref_disciplina_ofer
     FROM
         matricula a, pessoas b
     WHERE
@@ -138,17 +87,29 @@ SELECT * FROM (
             SELECT
                 id from disciplinas_ofer
             WHERE
-                fl_concluida = 't' AND
                 is_cancelada = '0' AND
                 ref_curso = $curso AND
-                turma = '$turma' and ref_periodo = '$periodo'
+                ref_periodo = '$periodo'
         ) AND
         a.ref_pessoa = b.id AND
+        a.ref_pessoa IN(
+            SELECT DISTINCT ref_pessoa
+            FROM contratos
+            WHERE
+                ref_curso = $curso AND
+                turma = '$turma'
+        ) AND
         a.ref_motivo_matricula = '0'
-) AS T1
-ORDER BY lower(to_ascii(nome)), ref_disciplina_ofer";
-*/
 
+        ) AND
+    is_cancelada = '0'
+ORDER BY diario;";
+
+$arr_legenda = $conn->get_all($sql_legenda);
+
+/**
+ * Consulta principal
+ */
 $sql_rel = "
 SELECT * FROM (
     SELECT DISTINCT
@@ -161,7 +122,7 @@ SELECT * FROM (
             SELECT
                 id from disciplinas_ofer
             WHERE
-                fl_concluida = 't' AND
+                fl_digitada = 't' AND
                 is_cancelada = '0' AND
                 ref_curso = $curso AND
                 ref_periodo = '$periodo'
@@ -209,20 +170,24 @@ $num_alunos  = count($arr_alunosid);
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
         <title>SA</title>
         <link href="<?=$BASE_URL?>public/styles/relatorio.css" rel="stylesheet" type="text/css">
-        <link href="<?=$BASE_URL?>public/styles/print.css" rel="stylesheet" type="text/css" media="print" />        
+        <link href="<?=$BASE_URL?>public/styles/print.css" rel="stylesheet" type="text/css" media="print" />
     </head>
     <body>
-        <div align="left">
-            <?php echo $header->get_empresa($PATH_IMAGES); ?>
-            <h2>Resumo de notas e faltas do curso no per&iacute;odo</h2>
+        <?php echo $header->get_empresa($PATH_IMAGES); ?>
+        <h2>Resumo de notas e faltas do curso no per&iacute;odo</h2>
+        <p>
+            <b style="color:red;">Atenção:</b><br />
+            Este relatório exibe somente os diários concluídos ou finalizados.
+            Lista todos os alunos com matrícula no período
+            selecionado e que tenham vínculo no curso/turma selecionados,
+            independente da situação atual do aluno.
+        </p>
         <strong>Per&iacute;odo:</strong> <?php echo $desc_periodo; ?><br />
         <strong>Curso:</strong> <?php echo $desc_curso[0]; ?><br />
         <strong>Turma:</strong> <?php echo $turma; ?><br />
         <strong>Campus:</strong> <?php echo $desc_curso[1]; ?><br />
         <br />
-
         <b>LEGENDA</b>
-
         <table cellpadding="0" cellspacing="0" class="relato">
             <tr>
                 <th align="center"><strong>C&oacute;d. Di&aacute;rio</strong></th>
@@ -233,9 +198,7 @@ $num_alunos  = count($arr_alunosid);
                 <th align="center"><strong>N Distribuida</strong></th>
                 <th align="center"><strong>Situa&ccedil;&atilde;o</strong></th>
             </tr>
-
             <?php foreach($arr_legenda as $legenda) : ?>
-
             <tr>
                 <td align="center"><?=$legenda['diario']?></td>
                 <td><?=$legenda['id']?> - <?=$legenda['descricao_extenso']?></td>
@@ -274,53 +237,40 @@ $num_alunos  = count($arr_alunosid);
                         ?>
                 </td>
                 <td>
-                    <?php
-                    //Situacao do diario
-                    if($legenda['fl_concluida'] == 't') {
-                        echo '<font color="red"><b>Finalizado</b></font>';
-                    }
-                    elseif($legenda['fl_concluida'] == 'f' AND $legenda['fl_digitada'] == 't'){
-                        echo '<font color="blue"><b>Conclu&iacute;do</b></font>';
-                    }
-                    elseif($legenda['fl_concluida'] == 'f' AND $legenda['fl_digitada'] == 'f') {
-                        echo '<font color="green"><b>Aberto</b></font>';
-                    }
-                    ?>
+                        <?php
+                        //Situacao do diario
+                        if($legenda['fl_concluida'] == 't') {
+                            echo '<font color="red"><b>Finalizado</b></font>';
+                        }
+                        elseif($legenda['fl_concluida'] == 'f' AND $legenda['fl_digitada'] == 't') {
+                            echo '<font color="blue"><b>Conclu&iacute;do</b></font>';
+                        }
+                        elseif($legenda['fl_concluida'] == 'f' AND $legenda['fl_digitada'] == 'f') {
+                            echo '<font color="green"><b>Aberto</b></font>';
+                        }
+                        ?>
                 </td>
             </tr>
-
             <?php endforeach; ?>
-
         </table>
-        <br />
-        <br />
+        <br /><br />
         <strong>Aten&ccedil;&atilde;o:</strong> O relat&oacute;rio abaixo exibe somente os di&aacute;rio concluídos.
-        <br />
-        <br />
-        <!-- Tabela principal -->
+        <br /><br />
 
         <table cellpadding="0" cellspacing="0" class="relato">
             <tr>
                 <th rowspan="2">
                     <strong>Aluno</strong>
                 </th>
-
                 <?php foreach($arr_diarios as $diario) : ?>
-
                 <th colspan="2"><strong><?=$diario?></strong></th>
-
                 <?php endforeach; ?>
-
             </tr>
             <tr>
-
                 <?php for($i = 0; $i < $num_diarios; $i++): ?>
-
                 <th><strong>N</strong></th>
                 <th><strong>F</strong></th>
-
                 <?php endfor; ?>
-
             </tr>
 
             <?php foreach($arr_alunosid as $alunoid) : ?>
@@ -331,33 +281,33 @@ $num_alunos  = count($arr_alunosid);
                 </td>
                     <?php foreach($arr_diarios as $diario): ?>
                 <td>
-                            <?php
-                            foreach($arr_rel as $rel) {
-                                if($alunoid === $rel['matricula'] AND $diario === $rel['ref_disciplina_ofer'])
-                                    echo number_format($rel['nota_final'],2,',','.');
-                            }
-                            ?>&nbsp;
+                    <?php
+                    foreach($arr_rel as $rel) {
+                        if($alunoid === $rel['matricula'] AND $diario === $rel['ref_disciplina_ofer'])
+                            echo number_format($rel['nota_final'],2,',','.');
+                    }
+                    ?>&nbsp;
                 </td>
                 <td>
-                            <?php
-                            foreach($arr_rel as $rel) {
-                                if($alunoid === $rel['matricula'] AND $diario === $rel['ref_disciplina_ofer'])
-                                    echo $rel['num_faltas'];
-                            }
-                            ?>&nbsp;
+                    <?php
+                    foreach($arr_rel as $rel) {
+                        if($alunoid === $rel['matricula'] AND $diario === $rel['ref_disciplina_ofer'])
+                            echo $rel['num_faltas'];
+                    }
+                    ?>&nbsp;
                 </td>
-                    <?php endforeach; ?>
+                <?php endforeach; ?>
             </tr>
 
             <?php endforeach; ?>
 
         </table>
-<br />
-<div class="nao_imprime">
-  <input type="button" value="Imprimir" onClick="window.print()" />
-  &nbsp;&nbsp;&nbsp;
-  <a href="#" onclick="javascript:window.close();">Fechar</a>
-</div>
-<br />
+        <br />
+        <div class="nao_imprime">
+            <input type="button" value="Imprimir" onClick="window.print()" />
+            &nbsp;&nbsp;&nbsp;
+            <a href="#" onclick="javascript:window.close();">Fechar</a>
+        </div>
+        <br />
     </body>
 </html>
