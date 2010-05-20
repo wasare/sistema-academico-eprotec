1b77351f252c6839ae98b027afc8df403c9d5124
wanderson <wasare@users.noreply.github.com>
2010-05-20 19:37:47 +0000

    - prepara arquivos para publicação no google code

--- config/sql/atualiza_banco-0.1.0_to_0.2.0/7_grant_privileges.sql
+++
@@ -1,5 +0,0 @@
--- usuario aluno
-GRANT SELECT ON TABLE diario_notas TO "aluno";
-GRANT SELECT ON TABLE diario_formulas TO "aluno";
-GRANT SELECT ON TABLE contratos TO "aluno";
-GRANT access TO usrsagu;
